30f98f0f8b72190d88951cfa8e2fbfe4efcb9a91
essethon <x.xfzhao@gmail.com>
2020-09-23 13:17:45 +0800

    install vim-plug

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,3 +3,7 @@
 # Zinit
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 
+# Vim-Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
