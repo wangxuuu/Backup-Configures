853ac8845fe47bde6299c6ca44965de8817cea83
essethon <x.xfzhao@gmail.com>
2021-01-23 21:17:46 +0800

    add set linux script

---
+++ setup-linux.sh
@@ -0,0 +1,10 @@
+cp ./.zshrc-linux-simple ~/.zshrc
+cp ./.zsh_functions ~/.zsh_functions
+cp ./.vimrc ~/.vimrc
+cp ./.basic.vimrc ~/.basic.vimrc
+cp ./.plug.vimrc ~/.plug.vimrc
+cp ./.gitconfig ~/.gitconfig
+
+echo 'Please set gitconfig by'
+echo '  git config --global user.name <your name>'
+echo '  git config --global user.email <your email>'
