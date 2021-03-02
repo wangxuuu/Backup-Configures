bbfd57af75c6de7690ce54594e7c5002d9b0311f
essethon <x.xfzhao@gmail.com>
2021-03-02 19:29:23 +0800

    add linux simple

--- setup-linux-simple.sh
+++ setup-linux-simple.sh
@@ -1,8 +1,8 @@
-cp ./.zshrc-linux-nogui ~/.zshrc
+cp ./.zshrc-linux-simple ~/.zshrc
 cp ./.zsh_functions ~/.zsh_functions
 cp ./.vimrc ~/.vimrc
 cp ./.basic.vimrc ~/.basic.vimrc
-cp ./.plug.vimrc ~/.plug.vimrc
+# cp ./.plug.vimrc ~/.plug.vimrc
 cp ./.gitconfig ~/.gitconfig
 cp ./.tmux.conf ~/.tmux.conf
 
